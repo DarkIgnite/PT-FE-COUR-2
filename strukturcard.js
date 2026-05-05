fa6e3bf4c6add33cb78c5e6440806736b9c80b4b
DarkIgnite <ryanfadhila18@gmail.com>
2026-05-06 07:30:44 +0800

    Refactor comments in strukturcard.js for clarity and consistency

--- strukturcard.js
+++ strukturcard.js
@@ -1,9 +1,11 @@
+// fungsi untuk mengganti warna latar belakang
 function gantiWarna() {
   document.body.style.backgroundColor = 'grey';
 }
 
+// fungsi untuk menampilkan pesan
 function kirimData(event) {
-  event.preventDefault(); // biar halaman tidak refresh
+  event.preventDefault();
   let input = document.getElementById('inputNama').value;
   document.getElementById('hasil').innerText = input;
 }
